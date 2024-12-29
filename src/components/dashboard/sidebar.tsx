59807c3d833365f9cec32ec9a660917e1c4e295f
Ben Fornefeld <ben.fornefeld@gmail.com>
2024-12-29 01:12:16 +0100

    improve: colors

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -17,6 +17,7 @@ export default function Sidebar() {
   return (
     <aside className="relative flex w-56 flex-col gap-2">
       <DashboardNav />
+
       <TeamSelector />
       <div className="flex items-center gap-2 pr-2">
         <UserMenu />
@@ -26,10 +27,11 @@ export default function Sidebar() {
           <HeartPulse className="h-4 w-4 text-fg-300" />
         </Button>
       </div>
+
       {/* prevents hydration issues */}
       {window !== undefined && (
         <div
-          className="absolute right-0 h-full select-none font-mono text-fg-500"
+          className="absolute right-0 h-full select-none font-mono text-bg-300"
           aria-hidden="true"
         >
           {Array.from({ length: columns }).map((_, i) => (
