e54455c45127b2b610ccb99eb900c8ebddc014ae
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-01-08 19:06:06 +0100

    improve: storybook build mechanism

---
+++ scripts/build-storybook.ts
@@ -0,0 +1,23 @@
+import { execSync } from "child_process";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const shouldBuildStorybook =
+  process.env.NEXT_PUBLIC_EXPOSE_STORYBOOK &&
+  process.env.NEXT_PUBLIC_EXPOSE_STORYBOOK === "1";
+
+if (!shouldBuildStorybook) {
+  console.log(
+    "Skipping Storybook build - NEXT_PUBLIC_EXPOSE_STORYBOOK =",
+    shouldBuildStorybook,
+  );
+  process.exit(0);
+}
+
+try {
+  execSync("storybook build -o public/storybook", { stdio: "inherit" });
+} catch (error) {
+  console.error("Failed to build Storybook:", error);
+  process.exit(1);
+}
--- src/app/storybook/page.tsx
+++ src/app/storybook/page.tsx
@@ -1,12 +1,23 @@
+import { notFound } from "next/navigation";
+
 export const dynamic = "force-static";
 
 /*
   This page is used to serve the storybook build.
   It is served from the public/storybook directory.
 
-  Run `pnpm storybook:build` to build the storybook.
+  Storybook is built by the `prebuild` script in package.json.
+  Env. variable NEXT_PUBLIC_EXPOSE_STORYBOOK must be set to 1 to build & expose the storybook.
 */
 export default function StorybookPage() {
+  const shouldExposeStorybook =
+    process.env.NEXT_PUBLIC_EXPOSE_STORYBOOK &&
+    process.env.NEXT_PUBLIC_EXPOSE_STORYBOOK === "1";
+
+  if (!shouldExposeStorybook) {
+    throw notFound();
+  }
+
   return (
     <iframe
       src="/storybook/index.html"
