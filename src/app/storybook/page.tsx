bd68386ea567f0fbd46a6002e999b05304fc2c52
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-01-08 17:59:32 +0100

    improve: storybook serving

--- src/app/storybook/page.tsx
+++ src/app/storybook/page.tsx
@@ -1,5 +1,11 @@
 export const dynamic = "force-static";
 
+/*
+  This page is used to serve the storybook build.
+  It is served from the public/storybook directory.
+
+  Run `pnpm storybook:build` to build the storybook.
+*/
 export default function StorybookPage() {
   return (
     <iframe
